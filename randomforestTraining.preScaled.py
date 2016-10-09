e4dd343883ef2856ab5e0868821775f55a25925a
kartong <kartongw@gmail.com>
2016-10-09 20:26:11 +0800

    completed model on all 3 different basic models

--- randomforestTraining.preScaled.py
+++ randomforestTraining.preScaled.py
@@ -1,11 +1,9 @@
 import scipy.misc as sm
 import numpy as np
-import glob
 import re
 import random
 import pandas as pd
-from skimage.transform import resize 
-from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
 import sys
 
 #FOLDER = "/media/3d352c20-c87e-44a3-8c27-e48382c61c7b/kaggle/train/"
@@ -15,12 +13,7 @@ LABELFILE = "/speed/kartong/kaggle/trainLabels.csv"
 
 
 TRAINING_SIZE = int(sys.argv[1])
-MODEL_TYPE 	  = sys.argv[2]
-PREDICTION_FILE = sys.argv[3]
-
-#TRAINING_SIZE = 100
-#MODEL_TYPE = "linear"
-#PREDICTION_FILE = "testResult.txt"
+PREDICTION_FILE = sys.argv[2]
 
 
 # Size of image to resize and use for training
@@ -97,9 +90,8 @@ def prepareImageForSVM(imageArr):
 #################################################
 #################################################
 
-numTraining = TRAINING_SIZE
-
 # Extract the data for training from 90% of the data
+numTraining = TRAINING_SIZE
 imageMultiArr = np.empty((TOTAL_PIXELS,0), dtype=int)
 for i in trainingImageIndex[1:numTraining]:
 	print "Reading Image %s" % i
@@ -108,17 +100,17 @@ for i in trainingImageIndex[1:numTraining]:
 	imageArrFlatten = np.ndarray.flatten(imageArr).reshape((TOTAL_PIXELS, 1))
 	imageMultiArr = np.append(imageMultiArr, imageArrFlatten, axis=1)
 
-clf = svm.SVR(kernel=MODEL_TYPE)
-clf.fit(imageMultiArr.T, list(df['level'][trainingImageIndex[1:numTraining]]))
 
-print clf.predict(imageMultiArr[:,6].reshape(1,-1))
+rf = RandomForestClassifier(n_estimators=100, n_jobs=100)
+rf.fit(imageMultiArr.T, list(df['level'][trainingImageIndex[1:numTraining]]))
+
 
 
 f = open(PREDICTION_FILE, "w")
 
 # Do test on the testing images
-#for i in testingImageIndex[0:200]:
-for i in testingImageIndex:
+for i in testingImageIndex[0:200]:
+#for i in testingImageIndex:
 #for i in range(1,200):
 	print "Testing Image %s" %i
 
@@ -127,16 +119,10 @@ for i in testingImageIndex:
 	print imagefile
 	testImageArr = readImageGreyscale(imagefile)
 	testImageArrFlatten = np.ndarray.flatten(testImageArr).reshape((TOTAL_PIXELS, 1))
-	#testImageArrPrep = prepareImageForSVM(testImageArr)
 	testImageArrPrep = testImageArrFlatten
-	#print testImageArrPrep
 	
 	# Prediction with the model
-	#print clf.predict(imageMultiArr[:,6)
-
-
-	predictedVal = clf.predict(testImageArrPrep.T)
-	print sum(sum(testImageArrPrep.T))
+	predictedVal = rf.predict(testImageArrPrep.T)
 	print predictedVal
 
 	# Write result to file
